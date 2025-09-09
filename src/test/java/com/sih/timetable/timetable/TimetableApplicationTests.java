1e99d6cfed1b94676010c6c682d41cca9bb51229
Pranjal Kundu <pranjal.physicslover.mtb@gmail.com>
2025-09-10 00:41:05 +0530

    Initial

---
+++ src/test/java/com/sih/timetable/timetable/TimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.timetable.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
